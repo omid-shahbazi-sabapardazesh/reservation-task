5bdb5d32e4a6bee5509bb4e2c8bda80e035d6daf
Omid <omidsh715@gmail.com>
2025-04-29 15:54:47 +0330

    Add cancel endpoint for reservation

--- app/api/views.py
+++ app/api/views.py
@@ -1,7 +1,11 @@
+from django.db import transaction
 from django_filters.rest_framework import FilterSet, DjangoFilterBackend
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import viewsets, status
+from rest_framework.views import APIView
+
 from .models import Table, Reservation
 from .serializers import TableSerializer, ReservationSerializer
 
@@ -27,3 +31,17 @@ class ReservationViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         Reservation.objects.filter(pk=instance.pk).update(canceled=True)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=True, methods=['post'], url_path='cancel')
+    def cancel_reservation(self, request, pk=None):
+        with transaction.atomic():
+            reservation = Reservation.objects.select_for_update().get(pk=pk)
+
+            if reservation.user != request.user or not request.user.is_owner:
+                return Response({'detail': 'Not allowed to cancel this reservation.'},
+                                status=status.HTTP_403_FORBIDDEN)
+
+            reservation.is_cancelled = True
+            reservation.save()
+
+            return Response({'status': 'Reservation cancelled'}, status=status.HTTP_200_OK)
