e2d1f45397439d8deb02a8ce8db5aa8d9925f3ef
omid-shahbazi-sabapardazesh <omid.shahbazi@sabapardazesh.net>
2025-12-22 10:53:16 +0330

    Update settings.py
    hard code secret key

--- app/core/settings.py
+++ app/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = "ss@v$o@k68f)yraj330aityf64g^r7%lgg1tw$)8#9s)pll=mt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
